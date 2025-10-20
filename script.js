1b3d0f6f29bdb013e296c20dbd123d959a067b96
michibish3 <michelle.w@cismoss.no>
2025-10-20 09:17:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const facts = [
   "I love painting, sleeping and being on my phone when I have free time. Sometimes, I also do baking when I have time, and the right ingredients",
-  "I dont do any sports in the moment, but I go to the gym instead.",
+  "I don't do any sports in the moment, but I go to the gym instead.",
   "Whenever I bake, I often like to make brownies, or cookies because those are my favourite things.",
   "I also enjoy being with friends after school."
 ];
@@ -12,4 +12,5 @@ button.addEventListener("click", () => {
   const randomIndex = Math.floor(Math.random() * facts.length);
   factParagraph.textContent = facts[randomIndex];
 });
+
 
